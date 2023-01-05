496de7ee6dc87136030f474cb7de90df33efcac9
Maria-Llaverias <llaverias.maria@gmail.com>
2023-01-06 00:46:37 +0100

    product component

---
+++ src/components/PerfumeCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../App.css";
+import AddToCartButton from "./AddToCartButton";
+import Pricetag from "./Pricetag";
+
+function PerfumeCard({ product, isDesktop }) {
+  return (
+    <div className="container">
+      {product &&
+        product.map((prod) => (
+          <div className="perfumeCard" key={prod.id}>
+            <div className="prodImage">
+              <img
+                src={isDesktop ? prod.imageDesktop : prod.imageMobile}
+                alt={prod.name}
+                key={prod.id}
+              />
+            </div>
+            <div className="prodDescription">
+              <h3>{prod.type}</h3>
+              <h1>{prod.name}</h1>
+              <p>{prod.description}</p>
+              <div className="priceTag">
+                <Pricetag
+                  price={prod.price}
+                  discount={prod.discount}
+                  dPrice={prod.dPrice}
+                ></Pricetag>
+              </div>
+              <div className="button">
+                <AddToCartButton> </AddToCartButton>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default PerfumeCard;
