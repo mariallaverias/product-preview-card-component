e61cc6d16dbd783a784d983b6736b97e1fe5f621
Maria-Llaverias <llaverias.maria@gmail.com>
2023-01-06 01:42:16 +0100

    testing link to fetch data

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ function App() {
 
   async function getData() {
     try {
-      const data = await axios(process.env.PUBLIC_URL + "/data.json");
+      const data = await axios(
+        process.env.PUBLIC_URL + "/product-preview-card-component/data.json"
+      );
       setProduct(data.data);
     } catch (err) {
       setError(err.message);
