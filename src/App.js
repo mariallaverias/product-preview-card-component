642022f3b7e924aa25994228b6778f0b0448b60f
Maria-Llaverias <llaverias.maria@gmail.com>
2023-01-06 01:01:02 +0100

    credit of code snippet

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ function App() {
   const [isDesktop, setDesktop] = useState(window.innerWidth > 800);
   const [error, setError] = useState("");
 
+  //credit of code snippet for viewport size: https://thewebdev.info/2021/11/20/how-to-conditionally-render-items-based-on-viewport-size-in-react/
+
   const updateMedia = () => {
     setDesktop(window.innerWidth > 800);
   };
-
   useEffect(() => {
     window.addEventListener("resize", updateMedia);
     return () => window.removeEventListener("resize", updateMedia);
   });
+
   useEffect(() => {
     getData();
   }, []);
@@ -23,7 +25,6 @@ function App() {
   async function getData() {
     try {
       const data = await axios("/data.json");
-      console.log(data);
       setProduct(data.data);
     } catch (err) {
       setError(err.message);
